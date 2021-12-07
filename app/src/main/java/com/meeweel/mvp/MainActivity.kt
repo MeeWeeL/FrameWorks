9a7b8f0ca783d99fae7e50b3bc7717f242f8a4b6
MeeWeeL <MeeWeeL@yandex.ru>
2021-12-07 19:47:44 +0300

    Counters MVP

--- app/src/main/java/com/meeweel/mvp/MainActivity.kt
+++ app/src/main/java/com/meeweel/mvp/MainActivity.kt
@@ -2,45 +2,51 @@ package com.meeweel.mvp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.meeweel.mvp.countersmvp.CountersPresenter
+import com.meeweel.mvp.countersmvp.CountersView
 import com.meeweel.mvp.databinding.ActivityMainBinding
 import com.meeweel.mvp.mvp.GreetingView
 import com.meeweel.mvp.mvp.MyPresenter
 
-class MainActivity : AppCompatActivity(), GreetingView {
+class MainActivity : AppCompatActivity(), GreetingView, CountersView {
 
     private lateinit var bind: ActivityMainBinding
     private val presenter: MyPresenter = MyPresenter()
-    private val counters = mutableListOf(0, 0, 0)
+    private val countersPresenter = CountersPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
         presenter.onAttach(this)
+
+        val listener = View.OnClickListener {
+            countersPresenter.counterClick(it.id)
+        }
         with (bind) {
             mainActivityButton.setOnClickListener {
                 (if (mainActivityTextView.text.equals("First text")) "Second text" else "First text")
                     .also { mainActivityTextView.text = it }
             }
-        }
-        bind.mainActivityPresenterButton.setOnClickListener {
-            presenter.onButtonClick()
-        }
-        bind.mainActivityCounter1Button.setOnClickListener {
-            val newValue = ((++counters[0]).toString())
-            bind.mainActivityCounter1Button.text = newValue
-        }
-        bind.mainActivityCounter2Button.setOnClickListener {
-            val newValue = ((++counters[1]).toString())
-            bind.mainActivityCounter2Button.text = newValue
-        }
-        bind.mainActivityCounter3Button.setOnClickListener {
-            val newValue = ((++counters[2]).toString())
-            bind.mainActivityCounter3Button.text = newValue
+            mainActivityPresenterButton.setOnClickListener {
+                presenter.onButtonClick()
+            }
+            mainActivityCounter1Button.setOnClickListener(listener)
+            mainActivityCounter2Button.setOnClickListener(listener)
+            mainActivityCounter3Button.setOnClickListener(listener)
         }
     }
 
     override fun setGreeting(greeting: String) {
         bind.mainActivityPresenterButton.text = greeting
+    }
+
+    override fun setButton(index: Int, text: String) {
+        when (index) {
+            0 -> bind.mainActivityCounter1Button.text = text
+            1 -> bind.mainActivityCounter2Button.text = text
+            2 -> bind.mainActivityCounter3Button.text = text
+        }
     }
 }
---
+++ app/src/main/java/com/meeweel/mvp/countersmvp/CountersView.kt
@@ -0,0 +1,5 @@
+package com.meeweel.mvp.countersmvp
+
+interface CountersView {
+    fun setButton(index: Int, text: String)
+}
